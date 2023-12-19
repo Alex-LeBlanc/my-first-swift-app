2685c7a87b3a05430f32af95aab1f1df8ba9bf03
Alex-LeBlanc <A.leblanc@queensu.ca>
2023-12-18 17:33:57 -0700

    init commit

---
+++ myFirstSwiftApp/myFirstSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstSwiftAppApp.swift
+//  myFirstSwiftApp
+//
+//  Created by Alex Le Blanc on 2023-12-15.
+//
+
+import SwiftUI
+
+@main
+struct myFirstSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
