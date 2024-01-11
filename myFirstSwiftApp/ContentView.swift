c44c5e6e8b4fd0e8242d520714658c7331d4135a
Alex-LeBlanc <A.leblanc@queensu.ca>
2024-01-10 22:53:47 -0500

    Completed Stanford CS193p 2023 lecture 1

--- myFirstSwiftApp/ContentView.swift
+++ myFirstSwiftApp/ContentView.swift
@@ -7,17 +7,67 @@
 
 import SwiftUI
 
+
+// ContentView BEHAVES like a View
 struct ContentView: View {
+    
+    
+    //   Stored/normal propertie
+    //    i IS N Int
+//    var i: Int
+//    var s: String
+//    
+    
+    //    Computed property. computed every time it is ran
+    //    some: the var has/can be any type, as long as it behaves like a view
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CardRowView()
+            CardRowView()
+            CardView()
         }
+       
         .padding()
+        .foregroundColor(Color.brown)
+        
     }
 }
+
+
+
+struct CardRowView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+        }
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder(lineWidth: 5)
+                Text("🚵‍♂️")
+                    .font(.largeTitle)
+                
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+//                    .foregroundColor(.brown)
+            }
+        })
+    }
+}
+
+
+
+
 
 #Preview {
     ContentView()
